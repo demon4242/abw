2f03e6198196cc844d57b6e8214826fed5607969
lesha demyanovich <demo_xxx@mail.ru>
2013-09-20 14:11:26 +0300

    initial commit

---
+++ abw/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace abw.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult SparesCatalogue()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+
+		public ActionResult Contacts()
+		{
+			return View();
+		}
+
+		public ActionResult DeliveryAndPayment()
+		{
+			return View();
+		}
+	}
+}
---
+++ abw/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="abw.MvcApplication" Language="C#" %>
