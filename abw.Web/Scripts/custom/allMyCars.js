30b9d543ecad81c953c30ee7990d8d38bb3b7e9f
Alexey Demyanovich <demo_xxx@mail.ru>
2014-04-13 15:51:42 +0300

    grids css update

---
+++ abw.Web/Scripts/custom/allMyCars.js
@@ -0,0 +1,12 @@
+﻿define(['knockout'],
+function (ko) {
+	// todo: make common module with 'allCars.js'
+	function allMyCars(viewModel) {
+		var cars = viewModel.list;
+		viewModel.list = ko.observableArray(cars);
+
+		ko.applyBindings(viewModel);
+	};
+
+	return allMyCars;
+});
