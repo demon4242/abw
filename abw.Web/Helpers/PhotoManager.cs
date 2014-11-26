e1f4eea280bce3ceb1830b867e5d8d615a8f6f07
Alexey Demyanovich <alexey.demyanovich@itechart-group.com>
2014-11-26 16:31:45 +0300

    remove redundant namespace reference

--- abw.Web/Helpers/PhotoManager.cs
+++ abw.Web/Helpers/PhotoManager.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
-using System.Web.Configuration;
 using abw.DAL.Entities;
 using abw.Logging;
 using abw.ViewModels;
