6a543f84fb02828e4aac2e995089107775bc2c5c
Alexey Demyanovich <demo_xxx@mail.ru>
2014-12-21 13:13:16 +0300

    ErrorsController has been introduced

---
+++ abw.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace abw.Controllers
+{
+	public class ErrorsController : Controller
+	{
+		public ActionResult GenericError()
+		{
+			return View();
+		}
+
+		public ActionResult PageNotFound()
+		{
+			return View();
+		}
+
+		public ActionResult InternalServerError()
+		{
+			return View();
+		}
+	}
+}
