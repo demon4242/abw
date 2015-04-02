4b66fbdc06543f07b0791bbd46a9c9d48899a8be
Alexey Demyanovich <demo_xxx@mail.ru>
2015-04-02 10:04:03 +0300

    fotorama smooth loading refactoring

---
+++ abw.Web/Scripts/custom/modules/initFotorama.js
@@ -0,0 +1,15 @@
+﻿define(['jquery',
+		'fotorama'],
+function ($) {
+	'use strict';
+
+	function initFotorama() {
+		// jquery selector is required
+		$('.fotorama').on('fotorama:ready', function (event) {
+			var element = event.target || event.srcElement;
+			element.style.visibility = 'visible';
+		});
+	}
+
+	return initFotorama();
+});
