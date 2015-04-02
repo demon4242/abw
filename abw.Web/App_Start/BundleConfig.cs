17971e942be0b2da02bddd8f65b2c4b33840c645
Alexey Demyanovich <demo_xxx@mail.ru>
2015-04-02 10:29:57 +0300

    styles minification has been added fix #2

--- abw.Web/App_Start/BundleConfig.cs
+++ abw.Web/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace abw.App_Start
 {
 	public class BundleConfig
 	{
-		// todo: styles are not minimized
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			const string stylesDirectory = "~/Content/styles/";
@@ -13,26 +12,37 @@ namespace abw.App_Start
 
 			Bundle bootstrapStyles = new CustomStyleBundle(stylesDirectory + "bootstrap/css")
 				.IncludeDirectory(stylesDirectory + "bootstrap", "*.css");
-			bundles.Add(bootstrapStyles);
+			AddBundle(ref bundles, bootstrapStyles);
+
+			Bundle fotoramaStyles = new CustomStyleBundle(stylesDirectory + "fotorama")
+				.Include(stylesDirectory + "fotorama.css");
+			AddBundle(ref bundles, fotoramaStyles);
 
 			Bundle globalStyles = new CustomStyleBundle(customStylesDirectory + "global")
 				.Include(customStylesDirectory + "global.less")
 				.Include(customStylesDirectory + "notifications.less")
 				.Include(customStylesDirectory + "spinner.less")
 				.Include(customStylesDirectory + "forms.less");
-			bundles.Add(globalStyles);
+			globalStyles.Transforms.Add(new CssMinify());
+			AddBundle(ref bundles, globalStyles);
 
 			Bundle carStyles = new CustomStyleBundle(customStylesDirectory + "car")
 				.Include(customStylesDirectory + "car.less");
-			bundles.Add(carStyles);
+			AddBundle(ref bundles, carStyles);
 
 			Bundle gridStyles = new CustomStyleBundle(customStylesDirectory + "grid")
 				.Include(customStylesDirectory + "grid.less");
-			bundles.Add(gridStyles);
+			AddBundle(ref bundles, gridStyles);
 
 			Bundle contactsStyles = new CustomStyleBundle(customStylesDirectory + "contacts")
 				.Include(customStylesDirectory + "contacts.less");
-			bundles.Add(contactsStyles);
+			AddBundle(ref bundles, contactsStyles);
+		}
+
+		private static void AddBundle(ref BundleCollection bundles, Bundle bundle)
+		{
+			bundle.Transforms.Add(new CssMinify());
+			bundles.Add(bundle);
 		}
 	}
 }
